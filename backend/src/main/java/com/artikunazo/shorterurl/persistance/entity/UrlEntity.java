20c8a059cd3690ffae882f9ccab2e6ee50fc47fa
Arturo <artik16032011@gmail.com>
2025-10-15 22:16:40 -0600

    Install lombok in entity

--- backend/src/main/java/com/artikunazo/shorterurl/persistance/entity/UrlEntity.java
+++ backend/src/main/java/com/artikunazo/shorterurl/persistance/entity/UrlEntity.java
@@ -4,8 +4,10 @@ import jakarta.persistence.*;
 
 import java.sql.Timestamp;
 
+@Data
 @Entity
 @Table(name = "urls")
+@DynamicUpdate
 public class UrlEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,46 +20,8 @@ public class UrlEntity {
     private String originalUrl;
 
     private String date;
+
+    @Column(name = "app_user")
     private String user;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getShortedUrl() {
-        return shortedUrl;
-    }
-
-    public void setShortedUrl(String shortedUrl) {
-        this.shortedUrl = shortedUrl;
-    }
-
-    public String getOriginalUrl() {
-        return originalUrl;
-    }
-
-    public void setOriginalUrl(String originalUrl) {
-        this.originalUrl = originalUrl;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getUser() {
-        return this.user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
 }
 
