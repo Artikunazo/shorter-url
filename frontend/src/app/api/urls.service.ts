d2e4dc0721bd3aeced2202e22ea7eeeb87e0ba05
Arturo <artik16032011@gmail.com>
2025-10-15 20:08:49 -0600

    Update environment vars in FE

--- frontend/src/app/api/urls.service.ts
+++ frontend/src/app/api/urls.service.ts
@@ -2,16 +2,18 @@ import {Injectable, inject} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UrlData} from '../models/urlData.model';
 import {Observable} from 'rxjs';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class UrlsService {
+	private readonly apiUrl = environment.apiUrl ?? 'http://localhost:8080/';
 	private readonly httpClient = inject(HttpClient);
 
 	requestNewShortUrl(data: UrlData): Observable<UrlData> {
 		return this.httpClient.post<UrlData>(
-			'http://localhost:8080/shorter-url/api/url/save',
+			this.apiUrl + 'shorter-url/api/url/save',
 			data,
 		);
 	}
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://shorter-url-y9ma.onrender.com/'
+};
